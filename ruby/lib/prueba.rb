2dc993851aab12324af367c45b507718551c6edc
Nato Szmedra <natoszmedra@gmail.com>
2019-04-15 10:20:13 -0300

    Punto 1 funca. TODO mucho refactor

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -7,51 +7,28 @@ class Module
     self.before = _before
     self.after = _after
 
-    # def self.method_added(method_name)
-    #   # pp 'self: ', self
-    #   pp 'hi. we ve got a new method: ' + method_name.to_s
-    #   if !@updated_methods || !@updated_methods.include?(method_name)
-    #     if !@updated_methods
-    #       @updated_methods = []
-    #     end
-    #
-    #     @updated_methods.push(method_name)
-    #
-    #     # TODO me gustaria ejecutar el method con este nombre, pero si lo saco de self, es Unbound!
-    #     # y si lo hago dentro de define method, el metodo ya se piso y no lo puedo ejecutar!
-    #
-    #     # self.define_method(method_name) {
-    #     #   pp 'simil antes'
-    #     #   ret = self.method(method_name).call
-    #     #   pp 'simil despues'
-    #     #   # ret
-    #     # }
-    #   end
-    # end
+    # TODO sacar esta anidacion!
+    def self.method_added(method_name)
+      pp 'hi. we ve got a new method: ' + method_name.to_s
+      if !@updated_methods || !@updated_methods.include?(method_name)
+        if !@updated_methods
+          @updated_methods = []
+        end
+
+        @updated_methods.push(method_name)
+
+        old_method = self.instance_method(method_name)
+
+        # TODO este metodo tiene que tener en su contexto los procs de before y after (de alguna forma mejor que esta)
+        self.define_method(method_name) {
+          self.class.before.call
+          ret = old_method.bind(self).call
+          self.class.after.call
+          ret
+        }
+      end
+    end
   end
-
-  # TODO esto no se esta llamando
-  # def new
-  #   pp 'hola'
-  # end
-
-  # def initialize
-  #   pp 'initializatin object'
-  #   pp self
-  #   super
-  # end
-
-end
-
-class Class
-  # def initialize
-  #   pp 'self in class', self
-  # end
-
-  # def new(*args, &block)
-  #   super
-  #   pp "new constructor defined"
-  # end
 end
 
 class Prueba
@@ -61,18 +38,7 @@ class Prueba
     proc { puts 'Despues del metodo' }
   )
 
-  def initialize
-    # TODO esto hay que hacerlo dinamico: en el initialize de c/objecto, buscar sus metodos y por cada uno:
-    self.define_singleton_method(:materia) do
-      self.singleton_class.superclass.before.call
-      ret = super()
-      self.singleton_class.superclass.after.call
-      ret
-    end
-  end
-
   def materia
-    # Prueba.before.call
     :tadp
   end
 end
