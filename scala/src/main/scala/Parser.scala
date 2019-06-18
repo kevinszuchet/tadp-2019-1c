9a07a4125a905d4bf9adaee271d3ca7c140ea945
Kevin Szuchet <kevinszuchet@gmail.com>
2019-06-18 15:55:22 -0300

    Agrego el parser char, medio tirado de los pelos

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -2,11 +2,15 @@ import scala.util.{Failure, Success, Try}
 
 case class Parser (input: String) {
   def anyChar: Try[ParserResult] = input.toList match {
-    case List () => Failure (new EmptyStringException)
-    case head :: tail => Success (new ParserResult (head.toString, tail.mkString) )
+    case List() => Failure (new EmptyStringException)
+    case head :: tail => Success (new ParserResult (head.toString, tail.mkString))
   }
+
+  def char(char: Char): Try[ParserResult] =
+    if (input.startsWith(char.toString) || input.isEmpty) this.anyChar else Failure(new CharacterNotFoundException(char, input))
 }
 
 
-class EmptyStringException() extends Exception()
+class EmptyStringException extends Exception
+class CharacterNotFoundException(char: Char, input: String) extends Exception("The character '$char' does not found in $input")
 case class ParserResult(parsedElement: String, notConsumed: String)
--- scala/src/test/scala/ParserTest.scala
+++ scala/src/test/scala/ParserTest.scala
@@ -8,8 +8,13 @@ class ParserTest extends FreeSpec with Matchers {
     actualResult shouldBe Success(expectedResult)
   }
 
+  // TODO ver de pasar la excepcion por parametro
   def assertEmptyString[T](actualResult: ⇒ T): Unit = {
     assertThrows[EmptyStringException](actualResult)
+  }
+
+  def assertNotFoundCharacter[T](actualResult: ⇒ T): Unit = {
+    assertThrows[CharacterNotFoundException](actualResult)
   }
 
   "Parsers" - {
@@ -21,6 +26,20 @@ class ParserTest extends FreeSpec with Matchers {
 
       "deberia fallar cuando el string es vacio" in {
         assertEmptyString(new Parser("").anyChar.get)
+      }
+    }
+
+    "char" - {
+      "deberia devolver un success de ParserResult(c, hau) cuando el string es chau y el caracter es c" in {
+        assertParserSucceededWithResult(new Parser("chau").char('c'), new ParserResult("c", "hau"))
+      }
+
+      "deberia fallar cuando el string es hola y el caracter es c" in {
+        assertNotFoundCharacter(new Parser("hola").char('c').get)
+      }
+
+      "deberia fallar cuando el string es vacio" in {
+        assertEmptyString(new Parser("").char('c').get)
       }
     }
   }
