b818c8fd37ab57da77e35a996654b5420df8bba4
Nato Szmedra <nato@producteca.com>
2019-06-21 00:21:06 -0300

    arranco a agregar letter parser. vamos a necesitar volver a los tipos parametricos (generics) y ver como hacemos con los combinators

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -2,6 +2,7 @@ import scala.util.{Failure, Success, Try}
 
 class EmptyStringException extends Exception
 class CharacterNotFoundException(char: Char, input: String) extends Exception("The character '$char' was not found in $input")
+class NotALetterException(input: String) extends Exception
 case class ParserResult(parsedElement: String, notConsumed: String)
 
 sealed trait ParserMixin {
@@ -33,22 +34,23 @@ case class char(char: Char) extends ParserMixin {
 
 }*/
 
-/*case object letter extends ParserInterface {
-  override def inputIsValid= (input: String) => super.inputIsValid(input).filter(_.head.isLetter)
-  def parseCriterion: String => Char = anyChar.parseCriterion
+case object letter extends ParserMixin {
+  def parseCriterion(input: String): Try[ParserResult] =
+    anyChar(input).filter(result => result.parsedElement.head.isLetter)
+      .orElse(Failure(new NotALetterException(input)))
 }
 
-case object digit extends ParserInterface {
+/*case object digit extends ParserMixin {
   override def inputIsValid= (input: String) => super.inputIsValid(input).filter(_.head.isDigit)
   def parseCriterion: String => Char = anyChar.parseCriterion
 }
 
-case object alphaNum extends ParserInterface {
+case object alphaNum extends ParserMixin {
   override def inputIsValid= (input: String) => super.inputIsValid(input).filter(_.head.isLetterOrDigit)
   def parseCriterion: String => Char = anyChar.parseCriterion
 }
 
-case class string(headSring: String) extends ParserInterface {
+case class string(headSring: String) extends ParserMixin {
   override def inputIsValid= (input: String) => super.inputIsValid(input).filter(_.contains(headSring))
   def parseCriterion: String => String = (input:String) => headSring
 }*/
