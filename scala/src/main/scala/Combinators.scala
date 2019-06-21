0094f547101125d47cd10307daa410fec538cfda
Nato Szmedra <nato@producteca.com>
2019-06-20 23:20:44 -0300

    Vuelo cosas que no compilan

--- scala/src/main/scala/Combinators.scala
+++ scala/src/main/scala/Combinators.scala
@@ -6,15 +6,8 @@ package object C {
 
   type Parser= String
 
-  case class Combinators{
+  object Combinators {
     def <|> : Parser => Parser => Parser = (parser1: Parser) => (parser2: Parser) => "parser3"
-
-    def sabee(parser1: Combinators): Combinators => Parser = (parser2: Combinators) => "holaa"
-
-    def a: Unit ={
-      val aob =  "a" sabee "b"
-      val a = <|>("a")("B")
-    }
   }
 
 }
