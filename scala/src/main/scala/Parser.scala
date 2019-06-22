a6ca39b30f28870d5da5c87e39d5fa54364f9afd
Kevin Szuchet <kevinszuchet@gmail.com>
2019-06-22 17:36:21 -0300

    Algunos refactors pelotudos

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -1,24 +1,22 @@
 import scala.util.{Failure, Success, Try}
 
 class EmptyStringException extends Exception
-class CharacterNotFoundException(char: Char, input: String)
-extends Exception(s"The character '$char' was not found in $input")
+class CharacterNotFoundException(char: Char, input: String) extends Exception(s"The character '$char' was not found in $input")
 class NotALetterException(input: String) extends Exception
 class NotADigitException(input: String) extends Exception
 class NotAnAlphaNumException(input: String) extends Exception
-class NotTheRightStringException(expectedString : String, currentString: String)
-extends Exception (s"Expected $expectedString... but got $currentString")
+class NotTheRightStringException(expectedString : String, currentString: String) extends Exception (s"Expected $expectedString... but got $currentString")
 
-object ParsersTypes{
+object ParsersTypes {
   //(parsedElement, notConsumed)
   type ParserOutput[T] = (T, String)
   type ParserResult[T] = Try[ParserOutput[T]]
 }
 import ParsersTypes._
 
-class Parser[T](parseCriterion: String => ParserResult[T]) {
+class Parser[T](criterion: String => ParserResult[T]) {
   def parseIfNotEmpty(input: String): ParserResult[T] =
-    if (input.isEmpty) Failure(new EmptyStringException) else parseCriterion(input)
+    if (input.isEmpty) Failure(new EmptyStringException) else criterion(input)
 
   def apply(input: String): ParserResult[T] = parseIfNotEmpty(input)
 
@@ -31,18 +29,17 @@ class Parser[T](parseCriterion: String => ParserResult[T]) {
         }
     )
 }
-case object anyChar extends Parser[Char](
-    input => Success((input.head, input.tail))
-) {}
+
+// TODO refactor para dejar todos inline (excepto string que no calienta). Todos haces casi lo mismo (ver transform)
+
+case object anyChar extends Parser[Char](input => Success((input.head, input.tail))) {}
 
 case class char(char: Char) extends Parser[Char](
-    input  => anyChar(input).filter(_._1 == char)
+    input => anyChar(input).filter(_._1 == char)
       .orElse(Failure(new CharacterNotFoundException(char, input)))
 ) {}
 
-case object void extends Parser[Unit](
-  input => Success(((), input.tail))
-) {}
+case object void extends Parser[Unit](input => Success(((), input.tail))) {}
 
 case object letter extends Parser[Char](
   input => anyChar(input).filter(_._1.isLetter)
@@ -59,11 +56,10 @@ case object alphaNum extends Parser[Char](
     .orElse(Failure(new NotAnAlphaNumException(input)))
 ) {}
 
-
-case class string(headString: String) extends Parser[String](
+case class string(string: String) extends Parser[String](
   input =>
-  if(input.startsWith(headString))
-    Success(headString, input.slice(headString.length, input.length))
-  else
-    Failure(new NotTheRightStringException(headString, input))
+    if (input.startsWith(string))
+      Success(string, input.slice(string.length, input.length))
+    else
+      Failure(new NotTheRightStringException(string, input))
 ) {}
