1ae1a720dfbf03540764216224af9e9d05667a21
Kevin Szuchet <kevinszuchet@gmail.com>
2019-06-25 10:45:31 -0300

    Le agrego la covarianza al tipo parametrico del parser (se perdio en el merge)

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -9,7 +9,7 @@ object ParserTypes {
 }
 import ParserTypes._
 
-class Parser[T](criterion: String => ParserResult[T]) {
+class Parser[+T](criterion: String => ParserResult[T]) {
   def apply(input: String): ParserResult[T] = criterion(input)
 
   def <|>[U >: T](anotherParser: Parser[U]) : Parser[U] = new Parser[U](input => this(input).orElse(anotherParser(input)))
@@ -23,7 +23,7 @@ class Parser[T](criterion: String => ParserResult[T]) {
   )
 
   def <~[U](anotherParser: Parser[U]): Parser[T] = new Parser[T](
-    this(_).flatMap{ case (parsedElement, notConsumed) => anotherParser(notConsumed).map(parserOutput => (parsedElement, parserOutput._2)) }
+    this(_).flatMap { case (parsedElement, notConsumed) => anotherParser(notConsumed).map(parserOutput => (parsedElement, parserOutput._2)) }
   )
   
   def satisfies(condition: ParserCondition[T]) = new Parser[T](input =>
@@ -31,7 +31,7 @@ class Parser[T](criterion: String => ParserResult[T]) {
   )
 
   def opt: Parser[Option[T]] = new Parser[Option[T]](
-    input => this(input).map{ case (parsedElement, notConsumed) => (Some(parsedElement), notConsumed) }.orElse(Success(None, input))
+    input => this(input).map { case (parsedElement, notConsumed) => (Some(parsedElement), notConsumed) }.orElse(Success(None, input))
   )
 
   def * : Parser[List[T]] = new Parser[List[T]](input =>
@@ -39,14 +39,14 @@ class Parser[T](criterion: String => ParserResult[T]) {
       {
         case (parsedElement, "") => Success(List(parsedElement), "")
         case (parsedElement, notConsumed) =>
-          this.*(notConsumed).map{ case (parsed, stillNotConsumed) => (parsedElement :: parsed, stillNotConsumed) } 
+          this.*(notConsumed).map { case (parsed, stillNotConsumed) => (parsedElement :: parsed, stillNotConsumed) }
       },
       _ => Success(List(), input)
     )
   )
 
   def + = new Parser[List[T]](
-    this(_).flatMap{ case (parsedElement, notConsumed) => this.*(notConsumed).map { case (parsed, stillNotConsumed) => (parsedElement :: parsed, stillNotConsumed) } }
+    this(_).flatMap { case (parsedElement, notConsumed) => this.*(notConsumed).map { case (parsed, stillNotConsumed) => (parsedElement :: parsed, stillNotConsumed) } }
   )
 }
 
@@ -57,11 +57,11 @@ class NonEmptyInputParser[T](criterion: String => ParserResult[T]) extends Parse
 
 case object anyChar extends NonEmptyInputParser[Char](input => Success(input.head, input.tail))
 
-class anyCharWithCondition(condition: ParserCondition[Char], exception: String => Throwable) extends NonEmptyInputParser[Char](input =>
-  anyChar.satisfies(condition)(input)
-    .orElse(Failure(exception(input))))
+class anyCharWithCondition(condition: ParserCondition[Char], exception: String => Throwable) extends NonEmptyInputParser[Char](
+  input => anyChar.satisfies(condition)(input).orElse(Failure(exception(input)))
+)
 
-case class char(char: Char) extends anyCharWithCondition(parsed => parsed == char, new CharacterNotFoundException(char, _))
+case class char(char: Char) extends anyCharWithCondition(_ == char, new CharacterNotFoundException(char, _))
 
 case object void extends NonEmptyInputParser[Unit](input => Success((), input.tail))
 
@@ -70,8 +70,7 @@ case object letter extends anyCharWithCondition(_.isLetter, new NotALetterExcept
 case object digit extends anyCharWithCondition(_.isDigit, new NotADigitException(_))
 
 case object alphaNum extends NonEmptyInputParser[Char](
-  input => (letter <|> digit)(input)
-    .orElse(Failure(new NotAnAlphaNumException(input)))
+  input => (letter <|> digit)(input).orElse(Failure(new NotAnAlphaNumException(input)))
 )
 
 case class string(string: String) extends Parser[String](
