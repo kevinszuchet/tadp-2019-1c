eb5f4a7bdf9c6ae84fe495283a1001c39dd327eb
Agustin Gomel <agomel97@gmail.com>
2019-06-30 16:11:08 -0300

    primera idea de caso practico silencios y sonidos

--- scala/src/main/scala/practicCase/MusicParser.scala
+++ scala/src/main/scala/practicCase/MusicParser.scala
@@ -1,37 +1,43 @@
-package practicCase
-
-import java.io.{PushbackReader, StringReader}
 import Musica._
-import scala.collection.mutable.ListBuffer
 
-case class Note(name: String)
+package object MusicParser {
 
-class MusicParser(input: String) {
-  protected val inputStream = new PushbackReader(new StringReader(input))
+  case object silencioParser extends Parser[Silencio](
+    ( char('_') <|> char('-') <|> char('~') )
+      .map{
+        case '_' => Silencio(Blanca)
+        case '-' => Silencio(Negra)
+        case '~' => Silencio(Corchea)
+      }(_)
+  )
 
-  protected def parseChar(): Char = {
-    val parsed = inputStream.read()
-    if (parsed == -1) throw new EOIParserException
-    return parsed.toChar
-  }
+  case object notaParser extends Parser[Nota](
+    anyChar
+      .map( charNota => Nota.notas.find(_.toString == charNota.toString).getOrElse(throw new NotANoteException(charNota)))
+      .<>( (char('#') <|> char('b')).opt )
+      .map{
+        case (nota, Some('#')) => nota.sostenido
+        case (nota, Some('b')) => nota.bemol
+        case (nota, None) => nota
+      }
+    (_)
+  )
 
-  protected def parseNote(): Nota = {
-    var next: Char = ' '
-    do next = parseChar() while (next == ' ')
-    Nota.notas.find(_.toString == next.toString()).getOrElse(throw new NotANoteException(next))
-  }
+  case object tonoParser extends Parser[Tono]( (integer <> notaParser).map{ case (octava, nota) => Tono(octava, nota)}(_) )
 
-  def parse(): List[Nota] = {
-    val result: ListBuffer[Nota] = ListBuffer()
-    try while (true)
-      result += parseNote()
-    catch {
-      case _: EOIParserException =>
-    }
-    return result.toList
-  }
+  case object figuraParser extends Parser[Figura](
+    ( string("1/16") <|> string("1/2") <|> string("1/4")  <|> string("1/8") <|> string("1/1") )
+      .map{
+        case "1/1" => Redonda
+        case "1/2" => Blanca
+        case "1/4" => Negra
+        case "1/8" => Corchea
+        case "1/16" => SemiCorchea
+      }(_)
+  )
+
+  case object sonidoParser extends Parser[Sonido](
+    ( tonoParser <> figuraParser ).map{ case (tono, figura) => Sonido(tono, figura) }(_)
+  )
+
 }
-
-class ParserException(reason: String) extends Exception(reason)
-class EOIParserException extends ParserException("reached end of input")
-class NotANoteException(val read: Char) extends ParserException(s"Expected [A|B|C|D|E|F|G] but got $read")
---
+++ scala/src/test/scala/practicCase/MusicParserTest.scala
@@ -0,0 +1,121 @@
+import MusicParser._
+import Musica._
+import org.scalatest.{FreeSpec, Matchers}
+
+import scala.util.{Failure, Success, Try}
+
+
+class MusicParserTest extends FreeSpec with Matchers {
+
+  def assertParserSucceededWithResult[T](actualResult: T, expectedResult: T): Unit = {
+    actualResult shouldBe Success(expectedResult)
+  }
+
+  def assertParserFailureAnyException[T](actualResult: Try[T]): Unit = {
+      actualResult.toOption shouldBe None
+  }
+
+
+  "Music Parsers" - {
+    "silencio" - {
+      "deberia devolver un silencio de Blanca y nada en lo no consumido" in {
+        assertParserSucceededWithResult(silencioParser("_"), (Silencio(Blanca), ""))
+      }
+      "deberia devolver un silencio de Blanca" in {
+        assertParserSucceededWithResult(silencioParser("_ el resto no parseado"), (Silencio(Blanca), " el resto no parseado"))
+      }
+      "deberia devolver un silencio de Negra" in {
+        assertParserSucceededWithResult(silencioParser("- el resto no parseado"), (Silencio(Negra), " el resto no parseado"))
+      }
+      "deberia devolver un silencio de corchea" in {
+        assertParserSucceededWithResult(silencioParser("~ el resto no parseado"), (Silencio(Corchea), " el resto no parseado"))
+      }
+      "deberia Fallar porque no puede parsear ningun silencio" in {
+        assertParserFailureAnyException(silencioParser("test"))
+      }
+    }
+
+    "nota" - {
+      "deberia devolver una nota A y nada en lo no consumido" in {
+        assertParserSucceededWithResult(notaParser("A"), (A, ""))
+      }
+      "deberia devolver una nota A" in {
+        assertParserSucceededWithResult(notaParser("A el resto no parseado"), (A, " el resto no parseado"))
+      }
+      "deberia devolver una nota B" in {
+        assertParserSucceededWithResult(notaParser("B el resto no parseado"), (B, " el resto no parseado"))
+      }
+      "deberia devolver una nota C" in {
+        assertParserSucceededWithResult(notaParser("C el resto no parseado"), (C, " el resto no parseado"))
+      }
+      "deberia devolver una nota C sostenido" in {
+        assertParserSucceededWithResult(notaParser("C# el resto no parseado"), (Cs, " el resto no parseado"))
+      }
+      "deberia devolver una nota G bemol" in {
+        assertParserSucceededWithResult(notaParser("Gb el resto no parseado"), (G.bemol, " el resto no parseado"))
+      }
+      "deberia Fallar porque no puede parsear ninguna Nota" in {
+        assertParserFailureAnyException(notaParser("test"))
+      }
+    }
+
+    "tono" - {
+      "deberia devolver una tono con octava 4 y nota A, y nada en lo no consumido" in {
+        assertParserSucceededWithResult(tonoParser("4A"), (Tono(4, A), ""))
+      }
+      "deberia devolver una tono con octava 9 y nota F" in {
+        assertParserSucceededWithResult(tonoParser("9F el resto no parseado"), (Tono(9, F), " el resto no parseado"))
+      }
+      "deberia devolver una tono con octava 148 y nota E" in {
+        assertParserSucceededWithResult(tonoParser("148E el resto no parseado"), (Tono(148, E), " el resto no parseado"))
+      }
+      "deberia Fallar porque no puede parsear ningun Tono" in {
+        assertParserFailureAnyException(tonoParser("test"))
+      }
+    }
+
+    "figura" - {
+      "deberia devolver una figura Redonda y nada en lo no consumido" in {
+        assertParserSucceededWithResult(figuraParser("1/1"), (Redonda, ""))
+      }
+      "deberia devolver una figura Redonda" in {
+        assertParserSucceededWithResult(figuraParser("1/1 el resto no parseado"), (Redonda, " el resto no parseado"))
+      }
+      "deberia devolver una figura Blanca" in {
+        assertParserSucceededWithResult(figuraParser("1/2 el resto no parseado"), (Blanca, " el resto no parseado"))
+      }
+      "deberia devolver una figura Negra" in {
+        assertParserSucceededWithResult(figuraParser("1/4 el resto no parseado"), (Negra, " el resto no parseado"))
+      }
+      "deberia devolver una figura Corchea" in {
+        assertParserSucceededWithResult(figuraParser("1/8 el resto no parseado"), (Corchea, " el resto no parseado"))
+      }
+      "deberia devolver una figura SemiCorchea" in {
+        assertParserSucceededWithResult(figuraParser("1/16 el resto no parseado"), (SemiCorchea, " el resto no parseado"))
+      }
+      "deberia Fallar porque no puede parsear ningun silencio" in {
+        assertParserFailureAnyException(figuraParser("test"))
+      }
+    }
+
+    "sonido" - {
+      "deberia devolver un sonido con tono(octava 4 y nota A) y figura redonda, y nada en lo no consumido" in {
+        assertParserSucceededWithResult(sonidoParser("4A1/1"), (Sonido(Tono(4, A), Redonda), ""))
+      }
+      "deberia devolver un sonido con tono(octava 4 y nota A) y figura redonda" in {
+        assertParserSucceededWithResult(sonidoParser("4A1/1 el resto no parseado"), (Sonido(Tono(4, A), Redonda), " el resto no parseado"))
+      }
+      "deberia devolver un sonido con tono(octava 99 y nota D) y figura semicorchea" in {
+        assertParserSucceededWithResult(sonidoParser("99D1/16 el resto no parseado"), (Sonido(Tono(99, D), SemiCorchea), " el resto no parseado"))
+      }
+      "deberia Fallar porque no puede parsear ningun Sonido" in {
+        assertParserFailureAnyException(sonidoParser("test"))
+      }
+      "debería ser un sonido para el cual la figura es Negra y el tono está compuesto por la octava 6 y la nota A sostenido" in {
+        assertParserSucceededWithResult(sonidoParser("6A#1/4"), (Sonido(Tono(6, As), Negra), ""))
+      }
+    }
+
+  }
+
+}
